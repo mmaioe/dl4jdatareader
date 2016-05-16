fb6ca74cec20f0bf983d72b4264e8ff6eaf51776
mmaioe <ito_m@ito-mao.local>
2016-05-16 11:16:03 +0900

    create dl4jdatasetiterator directory

---
+++ src/main/java/mmaioe/com/dl4jdatasetiterator/idx/Test.java
@@ -0,0 +1,7 @@
+package mmaioe.com.dl4jdatasetiterator.idx;
+
+/**
+ * Created by ito_m on 5/16/16.
+ */
+public class Test {
+}
--- src/test/scala/IDXTest.scala
+++ src/test/scala/IDXTest.scala
@@ -7,7 +7,7 @@ import org.nd4j.linalg.dataset.DataSet
 object IDXTest {
   def main(args: Array[String]) {
 
-    val path = "~/t10k-labels-idx1-ubyte.gz"
+    val path = "/Users/maoito/myProject/tensorflow/MNIST_data/t10k-labels-idx1-ubyte.gz"
     val dataset = IDXReader.read(path)
 
     while (dataset.hasNext()) {
